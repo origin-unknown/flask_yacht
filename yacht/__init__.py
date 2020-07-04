a1ed21ab93815a5d67124af61fa127bec642b8f7
origin-unknown <>
2020-07-04 02:30:21 +0200

    first test to implement the API endpoint

--- yacht/__init__.py
+++ yacht/__init__.py
@@ -6,6 +6,11 @@ from flask import Flask
 from flask import (
     jsonify
 )
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+db = SQLAlchemy()
+ma = Marshmallow()
 
 def create_app(env=os.getenv('FLASK_ENV', 'production')):
     app = Flask(__name__,
@@ -13,8 +18,8 @@ def create_app(env=os.getenv('FLASK_ENV', 'production')):
     )
     app.config.from_mapping(
         SECRET_KEY=b'\x09|\t\xe8V\xdb\x974{\x1aZz\xe9G\xea\x95\\xd6\xfa\xcf`\x7f\\*\n',
-        # SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(app.instance_path, f'{env}.sqlite'),
-        # SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(app.instance_path, f'{env}.sqlite'),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
 
     try:
@@ -22,6 +27,8 @@ def create_app(env=os.getenv('FLASK_ENV', 'production')):
     except OSError:
         pass
 
+    db.init_app(app)
+    ma.init_app(app)
 
     register_blueprints(app)
     register_endpoints(app)
@@ -31,8 +38,10 @@ def create_app(env=os.getenv('FLASK_ENV', 'production')):
 def register_blueprints(app):
     from .api.auth import create_module as api_auth_create_module
     from .api.main import create_module as api_main_create_module
+    from .api.docker.templates import create_module as api_docker_templates_create_module
     api_auth_create_module(app, url_prefix='/api')
     api_main_create_module(app, url_prefix='/api')
+    api_docker_templates_create_module(app, url_prefix='/api/templates')
 
 def register_endpoints(app):
     @app.route('/api/')
